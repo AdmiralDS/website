52b7a36875912fb63c6d2ab9898deea1ce6f1e0c
Anna Danshina <annetteblackat@gmail.com>
2024-03-01 17:30:37 +0300

    добавила заглушку на шрифтах

--- src/components/library/content/font-block/font-switcher.svelte
+++ src/components/library/content/font-block/font-switcher.svelte
@@ -8,8 +8,16 @@
   }
 </script>
 
+<!-- HTML comment syntax is valid in .astro files -->
+
 <div class="lib-font-block__font-tile">
-  <div class="lib-font-block__font-example">Admiral Semi Bold</div>
+  <div class={`lib-font-block__font-example
+    ${currentFontType === 'Subtitle' && 'lib-font-block__font-example--Subtitle'}
+    ${currentFontType === 'Body' && 'lib-font-block__font-example--Body'}
+    ${currentFontType === 'Caption' && 'lib-font-block__font-example--Caption'}
+  `}>
+    Admiral Semi Bold :)
+  </div>
   <div class="lib-font-block__font-buttons-container">
     {#each textTypeList as item}
       <Button on:click={() => handleTypeClick(item)} variant={`${item === currentFontType ? 'primary' : 'inactive'}`}
