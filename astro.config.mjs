62794809c5396891eca8fe2de650218d8868ee90
Andrey Litvinov <drune.email@gmail.com>
2024-03-04 13:37:10 +0300

    chore: upgrade Astro Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://AdmiralDS.github.io',
   base: '/website/',
-  integrations: [svelte()],
+  integrations: [svelte(), icon()],
 });
