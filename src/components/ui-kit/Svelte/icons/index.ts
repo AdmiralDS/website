97324ca68fc2fbe3282d1d4ccd2de35f7d8ce90d
Larisa Markova <lovemirror@rambler.ru>
2024-03-28 17:03:55 +0300

    Добавлена иконка HourglassOutline

--- src/components/ui-kit/Svelte/icons/index.ts
+++ src/components/ui-kit/Svelte/icons/index.ts
@@ -1,6 +1,7 @@
-export { default as IconArrowLeft } from "./arrow-left.svelte";
-export { default as IconArrowRight } from "./arrow-right.svelte";
-export { default as DragOutline } from "./drag_outline.svelte";
-export { default as EquallyOutline } from "./equally_outline.svelte";
-export { default as ErrorOutline } from "./error_outline.svelte";
-export { default as ErrorTriangleOutline } from "./error_triangle_outline.svelte";
+export { default as IconArrowLeft } from './arrow-left.svelte';
+export { default as IconArrowRight } from './arrow-right.svelte';
+export { default as DragOutline } from './drag_outline.svelte';
+export { default as EquallyOutline } from './equally_outline.svelte';
+export { default as ErrorOutline } from './error_outline.svelte';
+export { default as ErrorTriangleOutline } from './error_triangle_outline.svelte';
+export { default as HourglassOutline } from './hourglass-outline.svelte';
