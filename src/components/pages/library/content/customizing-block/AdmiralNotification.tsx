6d14d7323eeed0efc77019c5ce0ce6ee6db3a910
Larisa Markova <lovemirror@rambler.ru>
2024-04-18 17:15:43 +0300

    Удалены лишние отступы

--- src/components/pages/library/content/customizing-block/AdmiralNotification.tsx
+++ src/components/pages/library/content/customizing-block/AdmiralNotification.tsx
@@ -16,43 +16,43 @@ export const AdmiralNotifications = () => {
   const displayStatusIcon = true;
   const isClosable = true;
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
-      <div style={{ margin: '5px' }}>
-        <NotificationItem status="info" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
-          <NotificationItemTitle>{title}</NotificationItemTitle>
-          <NotificationItemContent>{body}</NotificationItemContent>
-          <NotificationItemButtonPanel>
-            <TextButton dimension="s" text={linkText} />
-          </NotificationItemButtonPanel>
-        </NotificationItem>
-      </div>
-      <div style={{ margin: '5px' }}>
-        <NotificationItem status="warning" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
-          <NotificationItemTitle>{title}</NotificationItemTitle>
-          <NotificationItemContent>{body}</NotificationItemContent>
-          <NotificationItemButtonPanel>
-            <TextButton dimension="s" text={linkText} />
-          </NotificationItemButtonPanel>
-        </NotificationItem>
-      </div>
-      <div style={{ margin: '5px' }}>
-        <NotificationItem status="error" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
-          <NotificationItemTitle>{title}</NotificationItemTitle>
-          <NotificationItemContent>{body}</NotificationItemContent>
-          <NotificationItemButtonPanel>
-            <TextButton dimension="s" text={linkText} />
-          </NotificationItemButtonPanel>
-        </NotificationItem>
-      </div>
-      <div style={{ margin: '5px' }}>
-        <NotificationItem status="success" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
-          <NotificationItemTitle>{title}</NotificationItemTitle>
-          <NotificationItemContent>{body}</NotificationItemContent>
-          <NotificationItemButtonPanel>
-            <TextButton dimension="s" text={linkText} />
-          </NotificationItemButtonPanel>
-        </NotificationItem>
-      </div>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: '5px',
+      }}
+    >
+      <NotificationItem status="info" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
+        <NotificationItemTitle>{title}</NotificationItemTitle>
+        <NotificationItemContent>{body}</NotificationItemContent>
+        <NotificationItemButtonPanel>
+          <TextButton dimension="s" text={linkText} />
+        </NotificationItemButtonPanel>
+      </NotificationItem>
+      <NotificationItem status="warning" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
+        <NotificationItemTitle>{title}</NotificationItemTitle>
+        <NotificationItemContent>{body}</NotificationItemContent>
+        <NotificationItemButtonPanel>
+          <TextButton dimension="s" text={linkText} />
+        </NotificationItemButtonPanel>
+      </NotificationItem>
+      <NotificationItem status="error" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
+        <NotificationItemTitle>{title}</NotificationItemTitle>
+        <NotificationItemContent>{body}</NotificationItemContent>
+        <NotificationItemButtonPanel>
+          <TextButton dimension="s" text={linkText} />
+        </NotificationItemButtonPanel>
+      </NotificationItem>
+      <NotificationItem status="success" displayStatusIcon={displayStatusIcon} isClosable={isClosable}>
+        <NotificationItemTitle>{title}</NotificationItemTitle>
+        <NotificationItemContent>{body}</NotificationItemContent>
+        <NotificationItemButtonPanel>
+          <TextButton dimension="s" text={linkText} />
+        </NotificationItemButtonPanel>
+      </NotificationItem>
     </div>
   );
 };
