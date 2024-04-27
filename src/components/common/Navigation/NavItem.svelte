58241dac9ae5f902fd9acec178a605683eece2e7
Anna Danshina <annetteblackat@gmail.com>
2024-04-27 10:23:56 +0300

    стили для навигации

--- src/components/common/Navigation/NavItem.svelte
+++ src/components/common/Navigation/NavItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { NavItemProps } from 'common';
+  import { beforeNavigate } from '$app/navigation';
 
   export let href: string;
   export let anchor: string;
@@ -13,6 +14,7 @@
   export let onClick: (key: string) => void;
 
   const handleClick = () => {
+    
     if (children.length > 0) {
       onToggleOpen?.(id);
       opened = !opened;
@@ -22,6 +24,11 @@
 
   $: link = href ? `${href}/#${anchor}` : `${import.meta.env.BASE_URL}#${anchor}`;
   $: id = href ? `${href}/#${anchor}` : `${anchor}`;
+
+  beforeNavigate(({ from, to }) => {
+    // отсутствует видимость link
+    // link = href ? `${href}/#${anchor}` : `${import.meta.env.BASE_URL}#${anchor}`;
+});
 </script>
 
 <a
@@ -29,13 +36,15 @@
   style="margin-left: {indent}px"
   class:active={currentActive === id}
   href={link}
-  on:click={handleClick}>{label}</a
+  on:click|preventDefault={handleClick}>{label}</a
 >
 
-{#if opened && children}
-  {#each children as child}
-    <svelte:self {...child} {currentActive} {onClick} indent={indent + 20} />
-  {/each}
+{#if currentActive === id && children}
+  <div class={`${opened ? "children-opened" : "children-closed"}`}>
+    {#each children as child}
+      <svelte:self {...child} {currentActive} {onClick} indent={indent + 20} />
+    {/each}
+  </div>
 {/if}
 
 <style>
