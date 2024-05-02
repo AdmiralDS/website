1e02b5dd9eafc3a5f54a3ea072202091e549e412
Larisa Markova <lovemirror@rambler.ru>
2024-05-02 16:09:25 +0300

    Добавлено открытие информации о релизе в модальном окне

--- src/components/organisms/releases/ReleaseCard.svelte
+++ src/components/organisms/releases/ReleaseCard.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
   import InfoBlockIcon from '@components/organisms/releases/InfoBlockIcon.svelte';
+  import { Modal, Tag } from '@components/ui-kit/Svelte';
 
   export let version: String = '';
   export let date: String = '';
   export let info: String = '';
+
+  let showModal = false;
 </script>
 
-<div class="release-card">
+<div
+  class="release-card"
+  on:click={() => {
+    showModal = true;
+    console.log(showModal);
+  }}
+>
   <div class="release-card__header">{version}</div>
   <div class="release-card__info">
     <InfoBlockIcon />
@@ -14,38 +23,28 @@
     <div class="release-card__info-separator"></div>
     {info}
   </div>
+
+  <Modal bind:showModal>
+    <div>
+      <div class="release-modal__name">Релиз v 2.1.5</div>
+      <div class="release-modal__date">6 июля 2023</div>
+      <div class="release-modal__separator"></div>
+      <div class="release-modal__name">Изменения</div>
+      <div class="release-modal__description">
+        Slider. Новый компонент в нашей библиотеке. Представляет собой элемент управления, помогает легко выбрать
+        значение, либо диапазон значений, перетаскивая ползунок по треку DropdownMenu/Multiselect. Добавлен параметр
+        hideSelectedOptions, отвечающий за сокрытие выбранных элементов в dropdownMenu Tag/TagGroup. Добавлено состояние
+        error для компонента Tag. Новый параметр invalidTags для TagGroup
+      </div>
+      <div class="release-modal__tag-container">
+        <Tag text="Slider" />
+        <Tag text="Multiselect" />
+        <Tag text="TagGroup" />
+      </div>
+    </div>
+  </Modal>
 </div>
 
 <style lang="css">
-  .release-card {
-    box-sizing: border-box;
-    padding: 10px;
-    height: 71px;
-    border-radius: 12px;
-    border: none;
-    background: var(--Light_blue, #f5f9fd);
-  }
-  .release-card__header {
-    color: var(--Dark_grey, #35383a);
-    font-family: Inter;
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 20px;
-    margin-bottom: 15px;
-  }
-  .release-card__info {
-    color: var(--Grey, #7a8190);
-    font-family: Inter;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 15px;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-  }
-  .release-card__info-separator {
-    background: var(--Light_grey, #e6eaf0);
-    width: 1px;
-    height: 12px;
-  }
+  @import './ReleaseCard.css';
 </style>
