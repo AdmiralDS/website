88c25b27c37db0fe71765feffa9aec97a1e0554d
Larisa Markova <lovemirror@rambler.ru>
2024-05-13 17:00:10 +0300

    Добавлено получение информации о релизах с npm

--- src/components/organisms/releases/ReleaseCard.svelte
+++ src/components/organisms/releases/ReleaseCard.svelte
@@ -1,39 +1,22 @@
 <script lang="ts">
   import InfoBlockIcon from '@components/organisms/releases/InfoBlockIcon.svelte';
-  import { Modal, Tag } from '@components/ui-kit/Svelte';
 
-  export let version: String = '';
-  export let date: String = '';
-  export let info: String = '';
-  export let description: String = '';
-
-  let showModal = false;
+  export let version: string = '';
+  export let date: string = '';
+  export let info: string = '';
+  export let link: string = 'https://github.com/AdmiralDS/react-ui/releases/tag/v8.3.0';
+  export let loading: boolean = true;
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
-<div class="release-card" on:click={() => (showModal = true)}>
+<div class="release-card" on:click={() => window.open(link, '_blanc')}>
   <div class="release-card__header">{version}</div>
   <div class="release-card__info">
     <InfoBlockIcon />
-    {date}
+    <div class="release-card__info-text">{date}</div>
     <div class="release-card__info-separator"></div>
-    {info}
+    <div class="release-card__info-text">{info}</div>
   </div>
-
-  <Modal bind:showModal>
-    <div>
-      <div class="release-modal__name">{version}</div>
-      <div class="release-modal__date">{date}</div>
-      <div class="release-modal__separator"></div>
-      <div class="release-modal__name">Изменения</div>
-      <div class="release-modal__description">{description}</div>
-      <div class="release-modal__tag-container">
-        <Tag text="Slider" />
-        <Tag text="Multiselect" />
-        <Tag text="TagGroup" />
-      </div>
-    </div>
-  </Modal>
 </div>
 
 <style lang="css">
--- src/components/organisms/releases/releases.svelte
+++ src/components/organisms/releases/releases.svelte
@@ -1,15 +1,52 @@
 <script>
+  import { onMount } from 'svelte';
   import { ArrowButtons } from '@components/ui-kit/Svelte';
   import ReleaseCard from '@components/organisms/releases/ReleaseCard.svelte';
   import ReleaseTitleCard from '@components/organisms/releases/ReleaseTitleCard.svelte';
-  import releasesList from './releases-list.json';
+
+  let releasesArray = [];
+  let rrr = [];
+  let loaded = false;
+
+  onMount(() => {
+    fetch('https://registry.npmjs.org/@admiral-ds/react-ui')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let versions = data.versions;
+        let times = data.time;
+        Object.keys(versions).forEach((version) => {
+          let versionTime = new Date(times[version])
+            .toLocaleString('ru', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })
+            .slice(0, -3);
+          releasesArray.unshift({
+            tag: version,
+            date: versionTime,
+            link: `https://github.com/AdmiralDS/react-ui/releases/tag/v${version}`,
+          });
+        });
+        rrr = releasesArray.slice(0, 3);
+        loaded = true;
+      });
+  });
 </script>
 
 <div>
   <div class="releases-wrapper">
     <ReleaseTitleCard />
-    {#each releasesList as release}
-      <ReleaseCard version={release.version} date={release.date} info={release.info} />
+    {#each rrr as release}
+      <ReleaseCard
+        version={`v ${release.tag}`}
+        date={release.date}
+        info="Релиз"
+        link={release.link}
+        loading={!loaded}
+      />
     {/each}
   </div>
   <ArrowButtons prevDisabled />
