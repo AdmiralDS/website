23084124cbb51fa4794af15153efea34b2a55bdb
Anna Danshina <annetteblackat@gmail.com>
2024-07-18 11:09:54 +0300

    боковое меню для планшета

--- src/components/molecules/NavigationPane/NavigationPane.svelte
+++ src/components/molecules/NavigationPane/NavigationPane.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   import Logo from './admiral.svg';
+  import LogoLight from './admiralLight.svg';
+  import Cross from './cross.svg';
+  import Lines from './lines.svg';
+  import { slide } from 'svelte/transition';
+  import { quintOut } from 'svelte/easing';
   import { onMount } from 'svelte';
   import { NavigationMenu, type NavigationItem } from '@components/molecules';
   import { joinAbsoluteUrlPath } from '@components/tools';
   import { type Page } from './types';
   import { goto, afterNavigate } from '$app/navigation';
   import { base as BASE_URL } from '$app/paths'
+
+  const TABLET_WIDTH = 1024;
+  $: innerWidth = 0;
+  $: isPaneOpened = false;
+
 
   export let items: Array<NavigationItem>;
   export let active: string | undefined;
@@ -73,13 +83,44 @@
     if (item.anchor) url.hash = item.anchor;
     goto(url);
   };
+
+  const handleClickOnMenuControl = () => isPaneOpened = !isPaneOpened;
+
+  function fadeSlide(node, options) {
+		const slideTrans = slide(node, options)
+		return {
+			duration: options.duration,
+			css: t => `
+				${slideTrans.css(t)}
+				opacity: ${t};
+			`
+		};
+	}
 </script>
 
-<nav class="side-nav">
+<svelte:window bind:innerWidth />
+
+{#if innerWidth === TABLET_WIDTH}
+  <div class="tablet-pane-container--closed" on:click={handleClickOnMenuControl}>
+    <Lines />
+    <LogoLight />
+  </div>
+{/if}
+
+<nav class={`${innerWidth === TABLET_WIDTH && !isPaneOpened ? 'side-nav--hidden' : 'side-nav'}`} transition:slide="{{delay: 250, duration: 300, easing: quintOut, axis: 'x'}}">
   <NavigationMenu {items} bind:active {opened} {onItemClick}>
-    <a href={BASE_URL} slot="logo">
-      <Logo fill="var(--Dark_grey, rgb(53, 56, 58))" />
-    </a>
+    <div slot="logo">
+      {#if innerWidth === TABLET_WIDTH && isPaneOpened}
+      <div class="tablet-pane-container--open" on:click={handleClickOnMenuControl}>
+        <Cross />
+        <Logo fill="#ffffff" />
+      </div>
+      {:else}
+      <a href={BASE_URL}>
+        <Logo fill="var(--Dark_grey, rgb(53, 56, 58))" />
+      </a>
+      {/if}
+  </div>
   </NavigationMenu>
 </nav>
 
