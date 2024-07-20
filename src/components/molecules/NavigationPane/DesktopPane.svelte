d35926236db1850ab06c01edb6e93bb8a2faa1c7
Alexandr Staloverov <syroszx@gmail.com>
2024-07-20 19:17:47 +0300

    Разделение NavigationPane на Tablet и Desktop

---
+++ src/components/molecules/NavigationPane/DesktopPane.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Pane from './Pane.svelte';
+  import Logo from './admiral.svg';
+  import { base as BASE_URL } from '$app/paths';
+</script>
+
+<Pane {...$$props}>
+  <a href={BASE_URL}>
+    <Logo fill="var(--Dark_grey, rgb(53, 56, 58))" />
+  </a>
+</Pane>
--- src/components/molecules/NavigationPane/index.ts
+++ src/components/molecules/NavigationPane/index.ts
@@ -1,1 +1,2 @@
-export { default as NavigationPane } from './NavigationPane.svelte';
+// export { default as NavigationPane } from './NavigationPane.svelte';
+export { default as NavigationPane } from './DesktopPane.svelte';
