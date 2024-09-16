7cbe207995129c0b5a6eec0f089fe75ff6912db7
Anna Danshina <annetteblackat@gmail.com>
2024-09-17 02:25:44 +0300

    castomizing block

--- src/components/pageComponents/library/Content/customizing-block/CustomizingBlock.svelte
+++ src/components/pageComponents/library/Content/customizing-block/CustomizingBlock.svelte
@@ -1,13 +1,24 @@
 <script>
 import { ContentBlock } from '@components/templates';
-import Switcher from './switcher/Switcher.svelte';
+import Switcher from './switcher/Switcher.svelte';  import { MOBILE_WIDTH } from '@components/const.ts';
+
+$: innerWidth = 0;
+
 import './customizingBlock.css';
 </script>
 
+<svelte:window bind:innerWidth />
+
 <ContentBlock id="customizing" title="КАСТОМИЗАЦИЯ">
   <div class="customizing__info">
-    Компоненты в библиотеке являются базовыми для<br />
-    системы и могут закрыть большинство потребностей<br />
+    Компоненты в библиотеке являются базовыми для
+    {#if innerWidth > MOBILE_WIDTH}
+      <br />
+    {/if}
+    системы и могут закрыть большинство потребностей
+    {#if innerWidth > MOBILE_WIDTH}
+      <br />
+    {/if}
     при проектировании интерфейсов
   </div>
   <div class="switcher-wrapper">
