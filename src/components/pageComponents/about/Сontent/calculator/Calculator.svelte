ca323ae7f510508ccc3c5f44ec622b25a067c883
Anna Danshina <annetteblackat@gmail.com>
2024-10-18 02:23:27 +0300

    calc logic

--- src/components/pageComponents/about/Сontent/calculator/Calculator.svelte
+++ src/components/pageComponents/about/Сontent/calculator/Calculator.svelte
@@ -3,8 +3,53 @@
   import { Toggle } from '@components/atoms';
   import Arrow from './Arrow.svelte';
   import { ContentBlock } from '@components/templates';
+  import Main from '@components/templates/Main/Main.svelte';
 
-  let result: number = 47;
+  const minResult = 30;
+  const maxResult = 49;
+
+  $: employeeCounter = 360;
+  const minEmployeeCounter = 10;
+  const maxEmployeeCounter = 1000;
+
+  $: priceCounter =  50;
+  const minPriceCounter = 10;
+  const maxPriceCounter = 180;
+
+  $: productCounter = 20;
+  const minProductCounter = 10;
+  const maxProductCounter = 50;
+
+  $: isMultiChannel = false;
+
+  let result: number = 30;
+
+  const handleToggleClick = () => isMultiChannel = !isMultiChannel;
+  const getTenPercentFromNumber = (num: number) => Math.ceil(num * 0.1);
+  
+  const makeCounting = (...args) => {
+    const range = maxResult - minResult;
+    const employeeRange = maxEmployeeCounter - minEmployeeCounter;
+    const priceRange = maxPriceCounter - minPriceCounter;
+    const productRange = maxProductCounter - minProductCounter;
+
+    const currentEmployeeCounter = Math.max(employeeCounter, minEmployeeCounter);
+    const currentEmployeeRangePoint = (currentEmployeeCounter - minEmployeeCounter) / employeeRange;
+
+    const currentPriceCounter = Math.max(priceCounter, minPriceCounter);
+    const currentPriceRangePoint = (currentPriceCounter - minPriceCounter)  / priceRange;
+
+    const currentProductCounter = Math.max(productCounter, minProductCounter);
+    const currentProductRangePoint = (currentProductCounter - minProductCounter) / productRange;
+
+    result = Math.ceil(minResult + (currentEmployeeRangePoint / 3 + currentPriceRangePoint / 3 + currentProductRangePoint / 3) * range);
+
+    if (isMultiChannel) {
+      result = getTenPercentFromNumber(result) + result;
+    }
+  }
+
+  $: makeCounting(employeeCounter, priceCounter, productCounter, isMultiChannel);
 </script>
 
 <ContentBlock>
@@ -13,12 +58,12 @@
       <div class="calculator-text">
         Используйте калькулятор для расчета вашей выгоды от внедрения дизайн-системы Адмирал
       </div>
-      <EmployeeSlider />
-      <PriceSlider />
-      <ProductSlider />
+      <EmployeeSlider bind:value={employeeCounter} />
+      <PriceSlider bind:value={priceCounter} />
+      <ProductSlider bind:value={productCounter} />
       <div class="calculator-multichannel-switcher">
         Вывод продукта сразу в несколько каналов
-        <Toggle />
+        <Toggle isChecked={isMultiChannel} on:click={handleToggleClick} />
       </div>
     </div>
     <div class="divider">
