ccc9c032ac7c0bf9d3dbead65c3a1b3858493638
Larisa Markova <lovemirror@rambler.ru>
2025-01-17 11:51:26 +0300

    Удалены переносы строк

--- src/components/pageComponents/about/Header/Header.svelte
+++ src/components/pageComponents/about/Header/Header.svelte
@@ -15,41 +15,11 @@
     <source src="Admiral_animation_video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
   </video>
   <div class="about-header__content" slot="content">
-    <div class="header-text"> АДМИРАЛ — 
+    <div class="header-text"> АДМИРАЛ —
         <br />
       это дизайн система</div>
     <div class="header__subtext">
-      Цель дизайн-системы — создать 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      единообразный и узнаваемый 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      визуальный стиль, который будет 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      использоваться во всех
-      продуктах и 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      сервисах компании. Это поможет 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      клиентам и партнерам быстрее узнавать 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      продукты компании, а также
-      привлекать 
-      {#if innerWidth <= MOBILE_WIDTH}
-        <br />
-      {/if}
-      новых пользователей и удерживать старых
+      Цель дизайн-системы — создать единообразный и узнаваемый визуальный стиль, который будет использоваться во всех продуктах и сервисах компании. Это поможет клиентам и партнерам быстрее узнавать продукты компании, а также привлекать новых пользователей и удерживать старых
     </div>
     <div class="cards-wrapper">
       <Card text="Ускоряет проектирование продуктов и упрощает разработку" icon={Arrow}/>
